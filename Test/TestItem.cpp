e991ea0c0447bf4da994dee10e875a3bcf0d81fb
Markantuono <niccolo.marcantuono@edu.unifi.it>
2026-02-07 11:58:34 +0100

    Unit testing
    Configurazione cmake e file per attuare lo unit testing

---
+++ Test/TestItem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 07/02/2026.
+//
+#include "gtest/gtest.h"
+#include "../Item.h"
---
+++ Test/runAllTest.cpp
@@ -0,0 +1,8 @@
+//
+// Created by HP on 07/02/2026.
+//
+#include "gtest/gtest.h"
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
